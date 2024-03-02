eb13fd814371c5bf32bbd2d80582c13ad36e8c7b
JamshidDev <raximovj662@gmail.com>
2024-03-02 11:49:00 +0500

    added button hidded

--- src/pages/MinFinPresenScreen/SixScreen.vue
+++ src/pages/MinFinPresenScreen/SixScreen.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full min-h-full relative flex justify-content-center align-items-center">
-    <img style="width:1500px;" src="@/assets/img/nst2.png" alt="">
+    <img style="width:1500px;" src="@/assets/img/nst3.png" alt="">
     
     <div class="visible-button" @click="$router.push({name:'vols'})"></div>
   </div>
