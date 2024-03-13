def7f6e3ac565887755501500f047bd43d32fd7b
Zafar Yodgorov <yodzafar9966@gmail.com>
2024-03-13 13:02:34 +0500

    fixed

--- src/pages/ThirteenScreen/ThirteenScreen.vue
+++ src/pages/ThirteenScreen/ThirteenScreen.vue
@@ -7,6 +7,36 @@ import TitleText from "../../components/TitleText/TitleText.vue";
 
 const animated = ref(false)
 const totalP = ref(45)
+const op1 = ref()
+const op2 = ref()
+const op3 = ref()
+const op4 = ref()
+const op5 = ref()
+const op6 = ref()
+
+function toggle(event, idx) {
+  switch (idx) {
+    case 1:
+      op1.value.toggle(event)
+      break
+    case 2:
+      op2.value.toggle(event)
+      break
+    case 3:
+      op3.value.toggle(event)
+      break
+    case 4:
+      op4.value.toggle(event)
+      break
+    case 5:
+      op5.value.toggle(event)
+      break
+    case 6:
+      op6.value.toggle(event)
+      break
+  }
+}
+
 
 const arr = [
   {
@@ -26,28 +56,69 @@ onMounted(() => {
 </script>
 
 <template>
+  <OverlayPanel ref='op1'>
+    <div class='max-w-20rem'>
+      <div class='text-lg font-medium'>Давлат Cолиқ қўмитаси</div>
+      <p class='mb-0'>Мижозлар рўйхатдан ўтиш жараёнида ишончли маълумотлар олиш.</p>
+    </div>
+  </OverlayPanel>
+  <OverlayPanel ref='op2'>
+    <div class='max-w-20rem'>
+      <div class='text-lg font-medium'>Марказий Банк</div>
+      <p class='mb-0'>Мижозлар томондан тўловлар ҳақида маълумот олиш.</p>
+    </div>
+  </OverlayPanel>
+  <OverlayPanel ref='op3'>
+    <div class='max-w-20rem'>
+      <div class='text-lg font-medium'>“Rail-Tarif” тизими</div>
+      <p class='mb-0'>Тарифларни «Rail-Tarif» сервер иловаси ёрдамида ҳисоблаш.</p>
+    </div>
+  </OverlayPanel>
+  <OverlayPanel ref='op4'>
+    <div class='max-w-20rem'>
+      <div class='text-lg font-medium'>“E-IMZO” тизими</div>
+      <p class='mb-0'>“E-IMZO” ёрдамида электрон ҳужжатларни имзолаш.</p>
+    </div>
+  </OverlayPanel>
+  <OverlayPanel ref='op5'>
+    <div class='max-w-20rem'>
+      <div class='text-lg font-medium'>“One-ID” тизими</div>
+      <p class='mb-0'>“One-ID” Веб - сайтларга фойдаланувчиларни аниқлаш имконини беради.</p>
+    </div>
+  </OverlayPanel>
+  <OverlayPanel ref='op6'>
+    <div class='max-w-20rem'>
+      <div class='text-lg font-medium'>Божхона қўмитаси</div>
+      <p class='mb-0'>Божхона қўмитаси ахборот тизими орқали интеграция ишлари олиб борилмоқда</p>
+    </div>
+  </OverlayPanel>
   <div class="w-full wrapper min-h-full flex flex-column justify-content-center align-items-center">
     <TitleText class='heading' title='Рақамлаштиришдан кейинги ҳолат'></TitleText>
     <PulseAnimation class='custom-pulse-animation overflow-visible' />
-    <PulseCenter class='pulse relative'>
+    <PulseCenter class='pulse'>
       <!--      <MobileDevice />-->
       <div class='relative text'>
         <div class="typing-demo">
           E-NAKL
         </div>
       </div>
-      <img class='oneid absolute' src='/images/one-id.png' alt='one-id'>
-      <img class='tax absolute' src='/images/soliq.png' alt='one-id'>
-      <img class='bank absolute' src='/images/milliy-bank.png' alt='one-id'>
-      <img class='railway absolute' src='/images/temir-yol-kengashi.png' alt='one-id'>
-      <img class='customs absolute' src='/images/bojxona.png' alt='one-id'>
+      <div class='h-full relative w-full z-5'>
+        <img class='oneid absolute cursor-pointer' src='/images/one-id.png' alt='one-id' @click='toggle($event, 5)'>
+        <img class='e-imzo absolute cursor-pointer' src='/images/e-imzo.png' alt='e-imzo' @click='toggle($event, 4)'>
+        <img class='tax absolute cursor-pointer' src='/images/soliq.png' alt='one-id' @click='toggle($event, 1)'>
+        <img class='bank absolute cursor-pointer' src='/images/milliy-bank.png' alt='one-id' @click='toggle($event, 2)'>
+        <img class='railway absolute cursor-pointer' src='/images/rail-tariff.png' alt='one-id'
+             @click='toggle($event, 3)'>
+        <img class='customs absolute cursor-pointer' src='/images/bojxona.png' alt='one-id' @click='toggle($event, 6)'>
+      </div>
+
     </PulseCenter>
     <div class='outline-wrapper'>
       <div class='relative'>
         <div v-for='(item, idx) in arr' :key='item.r' class='thumb-container'>
           <div class='seconds' :style='{transform: `rotate(${item.r}deg)`}'>
             <div class='thumb' :class='`thumb-${idx + 1}`'>
-              <img :src='`/images/${item.icon}`'>
+              <img :src='`/images/${item.icon}`' :alt='`icon-${idx}`'>
             </div>
             <div class='absolute box' :class='`box-${idx + 1}`'>
               <h6>{{ `${idx + 1}-Босқич` }}</h6>
@@ -78,7 +149,7 @@ onMounted(() => {
         </span>
       </div>
       <div class='text-2xl font-medium flex align-items-center gap-2'>
-        <img src='/images/user-type-success.png' alt='user-type'/>
+        <img src='/images/user-type-success.png' alt='user-type' />
         <span>
           Фойдаланувчилар тури – <span style='color:#11A832' class='font-semibold'>97</span>
         </span>
@@ -89,16 +160,41 @@ onMounted(() => {
           Мижозлар сони – <span style='color:#11A832' class='font-semibold'>8 836</span> нафар
         </span>
       </div>
-      <div class='text-2xl font-medium flex align-items-center gap-2'>
-        <img src='/images/time-success.png' alt='time' />
+    </div>
+    <div class='p-3 border-round-2xl border-3 bg-white flex flex-column gap-2 absolute total-info-right'
+         style='border-color: #11A832;'>
+      <div class='text-2xl font-medium flex align-items-center gap-2 mb-2'>
+        ЭРИШИЛГАН НАТИЖАЛАР
+      </div>
+      <div class='text-xl font-medium flex align-items-center gap-2'>
+        <img width='34' height='34' src='/images/time-success.png' alt='time' />
         <span>
-          Сарфланадиган вақт – <span style='color:#11A832' class='font-semibold'>12</span> соат
+          Темир йўл хизматларидан фойдаланишда мижознинг сарфланадиган вақти
+          <span style='color:#11A832' class='font-semibold'>60</span> соатга камайди
         </span>
       </div>
-      <div class='text-2xl font-medium flex align-items-center gap-2'>
-        <img src='/images/statistic-success.png' alt='statistic' />
+      <div class='text-xl font-medium flex align-items-center gap-2'>
+        <img width='34' height='34' src='/images/no-money-success.png' alt='time' />
         <span>
-          Йилига <span style='color:#11A832' class='font-semibold'>13 млрд. сўм </span> иқтисод
+          Бюрократик тўсиқлар ҳамда коррупцияни келтириб чиқарувчи омиллар бартараф этилди
+        </span>
+      </div>
+      <div class='text-xl font-medium flex align-items-center gap-2'>
+        <img width='34' height='34' src='/images/no-document-success.png' alt='time' />
+        <span>
+          Турли  хил темир йўл идораларига мурожаат этиш бартараф этилди
+        </span>
+      </div>
+      <div class='text-xl font-medium flex align-items-center gap-2'>
+        <img width='34' height='34' src='/images/check-sucess.png' alt='time' />
+        <span>
+          Кўрсатилаётган темир йўл хизматлари сифати яхшиланди
+        </span>
+      </div>
+      <div class='text-xl font-medium flex align-items-center gap-2'>
+        <img width='34' height='34' src='/images/statistic-success.png' alt='statistic' />
+        <span>
+          Йилига <span style='color:#11A832' class='font-semibold'>13 млрд. сўм </span> иқтисод қилинмоқда
         </span>
       </div>
     </div>
@@ -106,6 +202,15 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
+
+.total-info-right {
+  right: 40px;
+  bottom: 40px;
+  animation: 1.5s fadeIn forwards;
+  opacity: 0;
+  animation-delay: 5s;
+  max-width: 550px;
+}
 
 .pulse {
   justify-content: flex-start;
@@ -139,10 +244,20 @@ onMounted(() => {
   }
 }
 
+.e-imzo {
+  width: auto;
+  height: 25px;
+  z-index: 10;
+  left: 14%;
+  top: 54%;
+  animation: zoom-in-zoom-out 2.3s ease-out infinite;
+  filter: drop-shadow(1px 2px 1px rgba(0, 0, 0, .3));
+}
+
 .oneid {
-  left: 38%;
-  top: 45%;
-  width: 80px;
+  width: 86px;
+  left: 45%;
+  top: 17%;
   height: auto;
   z-index: 10;
   animation: zoom-in-zoom-out 2s ease-out infinite;
@@ -150,9 +265,9 @@ onMounted(() => {
 }
 
 .tax {
-  left: 30%;
-  top: 15%;
-  width: 75px;
+  left: 13%;
+  top: 30%;
+  width: 65px;
   height: auto;
   animation: zoom-in-zoom-out 2s ease-out infinite;
   animation-delay: 0.5s;
@@ -160,9 +275,9 @@ onMounted(() => {
 }
 
 .railway {
-  left: 10%;
-  top: 50%;
-  width: 70px;
+  left: 20%;
+  bottom: 10%;
+  width: 65px;
   height: auto;
   animation: zoom-in-zoom-out 2s ease-out infinite;
   animation-delay: 0.75s;
@@ -170,9 +285,9 @@ onMounted(() => {
 }
 
 .customs {
-  left: 50%;
-  top: 65%;
-  width: 75px;
+  right: 18%;
+  bottom: 10%;
+  width: 65px;
   height: auto;
   animation: zoom-in-zoom-out 4s ease-out infinite;
   animation-delay: 1s;
@@ -181,8 +296,8 @@ onMounted(() => {
 
 .bank {
   left: 70%;
-  top: 30%;
-  width: 70px;
+  top: 40%;
+  width: 65px;
   height: auto;
   animation: zoom-in-zoom-out 2s ease-out infinite;
   animation-delay: 1.5s;
@@ -199,8 +314,8 @@ onMounted(() => {
 }
 
 .total-info {
-  bottom: 50px;
-  left: 50px;
+  bottom: 40px;
+  left: 40px;
   animation: 1.5s fadeIn forwards;
   opacity: 0;
   animation-delay: 5s;
@@ -231,8 +346,8 @@ onMounted(() => {
   }
 
   &.box-2 {
-    left: 72%;
-    bottom: calc(100% + 160px);
+    left: 75%;
+    bottom: calc(100% + 140px);
     animation-delay: 2s;
     transform: rotate(-90deg);
   }
