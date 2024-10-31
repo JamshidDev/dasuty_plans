03571c0c10921e0d74d03298522051e4161abbd1
ismoildevuz <ismoildevuz@gmail.com>
2024-11-01 00:46:17 +0500

    feat: title

--- src/components/TitleText/TitleText.vue
+++ src/components/TitleText/TitleText.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-column absolute left-0 top-0 w-full pt-4">
-    <div class="gradient-background mx-auto text-center title text-4xl font-bold uppercase">
+    <div class="gradient-background mx-auto text-center title text-4xl font-bold uppercase" style="max-width: 1200px">
       {{title}}
     </div>
     <div class="gradient-background text-center subtitle mx-auto text-3xl uppercase">
