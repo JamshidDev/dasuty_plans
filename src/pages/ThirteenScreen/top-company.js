d900f43e39fa8ebec3d85a8039932ba53b638a9f
ismoildevuz <ismoildevuz@gmail.com>
2024-11-26 15:19:41 +0500

    feat: change top stats

--- src/pages/ThirteenScreen/top-company.js
+++ src/pages/ThirteenScreen/top-company.js
@@ -1,61 +1,54 @@
 export const topCompany = [
-  {
-    "id": 1152,
+    {
+        "id": 1152,
+        "name": `SixthPage.ThirdCardOption_1`,
+        "value": 3008
+    },
+    {
+        "id": 4644,
+        "name": `SixthPage.ThirdCardOption_2`,
+        "value": 2403
+    },
+    {
+        "id": 1186,
+        "name": `SixthPage.ThirdCardOption_3`,
+        "value": 2016
 
-    "name": `SixthPage.ThirdCardOption_1`,
-    "value": 1607
-  },
-  {
-    "id": 4644,
+    },
+    {
+        "id": 5485,
+        "name": `SixthPage.ThirdCardOption_4`,
+        "value": 1985
 
-    "name": `SixthPage.ThirdCardOption_2`,
-    "value": 1310
-  },
-  {
-    "id": 1186,
-
-    "name": `SixthPage.ThirdCardOption_3`,
-    "value": 977
-  },
-  {
-    "id": 5485,
-    "name": `SixthPage.ThirdCardOption_4`,
-    "value": 798
-  },
-  {
-    "id": 1386,
-
-    "name": `SixthPage.ThirdCardOption_5`,
-    "value": 518
-  },
-  {
-    "id": 9416,
-
-    "name": `SixthPage.ThirdCardOption_6`,
-    "value": 316
-  },
-  {
-    "id": 1388,
-
-    "name": `SixthPage.ThirdCardOption_7`,
-    "value": 303
-  },
-  {
-    "id": 9417,
-
-    "name": `SixthPage.ThirdCardOption_8`,
-    "value": 288
-  },
-  {
-    "id": 1587,
-
-    "name": `SixthPage.ThirdCardOption_9`,
-    "value": 224
-  },
-  {
-    "id": 1573,
-
-    "name": `SixthPage.ThirdCardOption_10`,
-    "value": 223
-  }
+    },
+    {
+        "id": 1386,
+        "name": `SixthPage.ThirdCardOption_5`,
+        "value": 1931
+    },
+    {
+        "id": 9416,
+        "name": `SixthPage.ThirdCardOption_6`,
+        "value": 1759
+    },
+    {
+        "id": 1388,
+        "name": `SixthPage.ThirdCardOption_7`,
+        "value": 1321
+    },
+    {
+        "id": 9417,
+        "name": `SixthPage.ThirdCardOption_8`,
+        "value": 946
+    },
+    {
+        "id": 1587,
+        "name": `SixthPage.ThirdCardOption_9`,
+        "value": 946
+    },
+    {
+        "id": 1573,
+        "name": `SixthPage.ThirdCardOption_10`,
+        "value": 836
+    }
 ]
